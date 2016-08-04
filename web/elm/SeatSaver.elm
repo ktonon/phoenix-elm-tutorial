468f738b392cfda9c180b5a5c4d6b27fcf735387
Kevin Tonon <kevin@betweenconcepts.com>
2016-08-03 20:24:16 -0400

    Converting to 2 space indentation

--- web/elm/SeatSaver.elm
+++ web/elm/SeatSaver.elm
@@ -6,36 +6,36 @@ import Html.Attributes exposing (class)
 
 main : Html a
 main =
-    view init
+  view init
 
 
 -- MODEL
 
 
 type alias Seat =
-    { seatNo : Int
-    , occupied : Bool
-    }
+  { seatNo : Int
+  , occupied : Bool
+  }
 
 type alias Model =
-    List Seat
+  List Seat
 
 
 init : Model
 init =
-    [ { seatNo = 1, occupied = False}
-    , { seatNo = 2, occupied = False}
-    , { seatNo = 3, occupied = False}
-    , { seatNo = 4, occupied = False}
-    , { seatNo = 5, occupied = False}
-    , { seatNo = 6, occupied = False}
-    , { seatNo = 7, occupied = False}
-    , { seatNo = 8, occupied = False}
-    , { seatNo = 9, occupied = False}
-    , { seatNo = 10, occupied = False}
-    , { seatNo = 11, occupied = False}
-    , { seatNo = 12, occupied = False}
-    ]
+  [ { seatNo = 1, occupied = False}
+  , { seatNo = 2, occupied = False}
+  , { seatNo = 3, occupied = False}
+  , { seatNo = 4, occupied = False}
+  , { seatNo = 5, occupied = False}
+  , { seatNo = 6, occupied = False}
+  , { seatNo = 7, occupied = False}
+  , { seatNo = 8, occupied = False}
+  , { seatNo = 9, occupied = False}
+  , { seatNo = 10, occupied = False}
+  , { seatNo = 11, occupied = False}
+  , { seatNo = 12, occupied = False}
+  ]
 
 
 -- VIEW
@@ -43,9 +43,9 @@ init =
 
 view : Model -> Html a
 view model =
-    ul [ class "seats" ] (List.map seatItem model)
+  ul [ class "seats" ] (List.map seatItem model)
 
 
 seatItem : Seat -> Html a
 seatItem seat =
-    li [ class "seat available" ] [ Html.text (toString seat.seatNo) ]
+  li [ class "seat available" ] [ Html.text (toString seat.seatNo) ]
